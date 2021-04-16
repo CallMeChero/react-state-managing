37d0f3d6acd08c7060850a65787eefed5a6a30a9
CallMeChero <david.ceranic@gmail.com>
2021-04-16 13:28:55 +0200

    Add: managing form state & errors

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Products from "./Products";
 import { Routes, Route } from "react-router-dom";
 import Cart from "./Cart";
 import Detail from "./Detail";
+import Checkout from "./Checkout";
 
 export default function App() {
   const [ cart, setCart ] = useState(() => {
@@ -28,7 +29,6 @@ export default function App() {
       if(itemInCart) {
         return items.map((i) => i.sku === sku ? {...i, quantity: i.quantity + 1} : i)
       } else {
-        // Return new  array with the new item appended
         return [...items, { id, sku, quantity: 1}];
       }
     })
@@ -43,6 +43,11 @@ export default function App() {
       })
   }
 
+  function emptyCart() {
+    // zbog principle of least privilege
+    setCart([]);
+  }
+
   return (
     <>
       <div className="content">
@@ -52,6 +57,7 @@ export default function App() {
             <Route path="/:category" element={<Products />}/>
             <Route path="/:category/:id" element={<Detail addToCart={addToCart} />}/>
             <Route path="/cart" element={<Cart cart={cart} updateQuantity={updateQuantity}/>}/>
+            <Route path="/checkout" element={<Checkout cart={cart} emptyCart={emptyCart} />}/>
           </Routes>
         </main>
       </div>
